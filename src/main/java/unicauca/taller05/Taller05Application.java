cd48d7f09b3206beef3d83166ea8e0e48ae0c4e1
maxskaink <mangelcvivas@unicauca.edu.co>
2025-09-21 21:39:42 -0500

    Initial commit - Taller05 ASAE project setup

---
+++ src/main/java/unicauca/taller05/Taller05Application.java
@@ -0,0 +1,13 @@
+package unicauca.taller05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taller05Application.class, args);
+    }
+
+}
---
+++ src/test/java/unicauca/taller05/Taller05ApplicationTests.java
@@ -0,0 +1,13 @@
+package unicauca.taller05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
