4a5f0c69e0248e147316c058e081770846ef2e66
maxskaink <mangelcvivas@unicauca.edu.co>
2025-10-19 20:52:03 -0500

    Solve aberration

--- src/main/java/unicauca/taller05/infraestructura/input/controllers/DocenteController.java
+++ src/main/java/unicauca/taller05/infraestructura/input/controllers/DocenteController.java
@@ -16,7 +16,7 @@ public class DocenteController {
     private final CUDocenteIn cuDocenteIn;
     private final ModelMapper modelMapper;
 
-    @PostMapping("/crear")
+    @PostMapping("")
     public DocenteDTORespuesta crearDocente(@RequestBody DocenteDTOPeticion docenteDTOPeticion) {
         Docente docente = modelMapper.map(docenteDTOPeticion, Docente.class);
         Docente docenteCreado = cuDocenteIn.crearDocente(docente);
